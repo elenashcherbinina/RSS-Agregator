68c5ac9f28229b5a9bab2dfb6e463179043cc207
Elena Shcherbinina <el.shcherbinina@outlook.com>
2023-05-05 16:05:00 +0300

    updated functions

--- src/app.js
+++ src/app.js
@@ -1,52 +1,71 @@
+import i18next from 'i18next';
 import onChange from 'on-change';
 import * as yup from 'yup';
+
+import resources from './locales/index.js';
 import render from './render.js';
 
 export default () => {
-  const elements = {
-    form: document.querySelector('form'),
-    input: document.querySelector('input'),
-    submitButton: document.querySelector('[type="submit"]'), // зачем она?
-    feedback: document.querySelector('.feedback'),
-    containerFeeds: document.querySelector('div.posts'),
-    containerPosts: document.querySelector('div.feeds'),
-  };
+  const i18nInstance = i18next.createInstance();
 
-  const state = {
-    form: {
-      state: 'filling',
-      valid: true,
-      error: null,
-    },
-    feeds: [],
-    posts: [],
-  };
+  i18nInstance
+    .init({
+      lng: 'ru',
+      debug: false,
+      resources,
+    })
+    .then(() => {
+      const elements = {
+        form: document.querySelector('form'),
+        input: document.querySelector('input'),
+        submitButton: document.querySelector('[type="submit"]'), // зачем она?
+        feedback: document.querySelector('.feedback'),
+        containerFeeds: document.querySelector('div.posts'),
+        containerPosts: document.querySelector('div.feeds'),
+      };
 
-  console.log(state);
+      const state = {
+        form: {
+          state: 'filling',
+          valid: true,
+          error: null,
+        },
+        feeds: [],
+        posts: [],
+      };
 
-  const watchedState = onChange(state, render(elements, state));
-
-  const schema = yup.string().url('Cсылка должна быть валидным URL').notOneOf(state.feeds, 'RSS уже существует');
-
-  elements.form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    watchedState.form.state = 'sending';
-    const formData = new FormData(e.target);
-    const curUrl = formData.get('url').trim();
-
-    schema
-      .validate(curUrl)
-      .then(() => {
-        watchedState.form.valid = 'true';
-        watchedState.form.error = '';
-        watchedState.feeds.push(curUrl);
-        watchedState.form.state = 'finished';
-      })
-      .catch((error) => {
-        watchedState.form.valid = 'false';
-        watchedState.form.error = error.message;
-        watchedState.form.state = 'failed';
+      yup.setLocale({
+        mixed: {
+          notOneOf: 'rssAlreadyExists',
+        },
+        string: {
+          url: 'invalidUrl',
+        },
       });
-    console.log(state);
-  });
+
+      const schema = yup.string().url().notOneOf(state.feeds);
+
+      const watchedState = onChange(state, render(elements, state, i18nInstance));
+
+      elements.form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        watchedState.form.state = 'sending';
+        const formData = new FormData(e.target);
+        const curUrl = formData.get('url').trim();
+
+        schema
+          .validate(curUrl)
+          .then(() => {
+            watchedState.form.valid = 'true';
+            watchedState.form.error = null;
+            watchedState.form.state = 'finished';
+            watchedState.feeds.push(curUrl);
+          })
+          .catch((error) => {
+            watchedState.form.valid = 'false';
+            watchedState.form.error = error.message;
+            watchedState.form.state = 'failed';
+          });
+      });
+    });
 };
--- src/render.js
+++ src/render.js
@@ -1,34 +1,32 @@
-const renderError = (elements, state) => {
+const renderError = (elements, error, i18nInstance) => {
   const { input, feedback } = elements;
   input.classList.add('is-invalid');
+  feedback.textContent = i18nInstance.t(`errors.${error}`);
   feedback.classList.add('text-danger');
-  feedback.textContent = state.form.error;
+  feedback.classList.remove('text-success');
 };
 
 const renderValidState = (elements) => {
   const { input, feedback, form } = elements;
-  form.reset();
-  input.focus();
   input.classList.remove('is-invalid');
   feedback.classList.remove('text-danger');
   feedback.classList.add('text-success');
   feedback.innerHTML = '';
+  form.reset();
+  input.focus();
 };
 
 // const renderFeeds = () => {};
 
 // const renderPosts = () => {};
 
-export default (elements, state) => (path, value) => {
+export default (elements, state, i18nInstance) => (path, value) => {
   switch (path) {
     case 'form.valid':
       if (value === 'true') {
         renderValidState(elements);
-        // renderFeeds();
-        // renderPosts();
-      }
-      if (value === 'false') {
-        renderError(elements, state);
+      } else {
+        renderError(elements, state.form.error, i18nInstance);
       }
       break;
     default:
