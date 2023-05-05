8ced14de9b0cdff491b367ed1b8782e4594c6024
Elena Shcherbinina <el.shcherbinina@outlook.com>
2023-05-05 16:03:40 +0300

    added locales for i18n

---
+++ src/locales/en.js
@@ -0,0 +1,12 @@
+export default {
+  translation: {
+    errors: {
+      invalidUrl: 'Link must be a valid URL',
+      rssAlreadyExists: 'RSS already exists',
+    },
+    containers: {
+      posts: '',
+      feeds: '',
+    },
+  },
+};
---
+++ src/locales/ru.js
@@ -0,0 +1,12 @@
+export default {
+  translation: {
+    errors: {
+      invalidUrl: 'Cсылка должна быть валидным URL',
+      rssAlreadyExists: 'RSS уже существует',
+    },
+    containers: {
+      posts: '',
+      feeds: '',
+    },
+  },
+};
