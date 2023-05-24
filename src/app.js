b2780c732edc7c0193581e74f4946c2ce5e02d5f
Elena Shcherbinina <el.shcherbinina@outlook.com>
2023-05-24 12:14:56 +0300

    new state for form

--- src/app.js
+++ src/app.js
@@ -64,7 +64,7 @@ export default () => {
 
       const state = {
         form: {
-          state: 'valid',
+          state: 'filling',
         },
         loadingProcess: {
           state: 'filling',
